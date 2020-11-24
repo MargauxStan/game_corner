c29ae95703d940b5d7ce0eb8c0e998e2f28fd09a
margaux stanislas <margaux.stanislas@hotmail.fr>
2020-11-24 11:58:58 +0100

    added view for index and new for games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :users
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :platform, presence: true
 end
