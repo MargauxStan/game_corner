7bd6eb6c738afa15bfc8a4ef92f9844ab1014982
margaux stanislas <margaux.stanislas@hotmail.fr>
2020-11-27 06:29:15 +0100

    calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import{ flatpickrFunc } from "../plugins/flatpickr"
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  flatpickrFunc();
 });
